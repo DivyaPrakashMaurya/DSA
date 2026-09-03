0568d40cbf7bb62d7a18084c222ad7c58fe79ddd
Divya Prakash Maurya <divyaprakashmaurya07@gmail.com>
2026-09-03 14:03:52 +0530

    Time: 1 ms (94.41%) | Memory: 42.7 MB (22.90%) - LeetSync

--- 4332-check-good-integer/check-good-integer.java
+++ 4332-check-good-integer/check-good-integer.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean checkGoodInteger(int n) {
-        int temp = n;
         int sum = 0;
         int sqSum = 0;
-        while(temp > 0){
-            int lastDig = temp % 10;
+        while(n > 0){
+            int lastDig = n % 10;
             sum += lastDig;
             sqSum += lastDig * lastDig;
-            temp = temp /10;
+            n = n /10;
         }
         if(sqSum - sum >= 50){
             return true;
