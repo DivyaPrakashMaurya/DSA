09dd13b68ecf39f0990979f01c4a8af8feb109c8
Divya Prakash Maurya <divyaprakashmaurya07@gmail.com>
2026-09-06 14:31:44 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (71.47%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -8,7 +8,9 @@ class Solution {
             list.add(dig);
             temp = temp / 10;
         }
+
         int max = Integer.MIN_VALUE;
+
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) {
                 int product = list.get(i) * list.get(j);
